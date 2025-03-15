02e3c312272f6afadc9ac7dd7e101c3208427952
Akhilesh Shende <akhilesh@skynetixsystems.com>
2025-03-15 11:47:10 +0530

    update note

--- serverless-cli/handler.js
+++ serverless-cli/handler.js
@@ -1,11 +1,18 @@
 'use strict';
 
-'use strict';
-
 export async function createNote(event) {
   return {
     statusCode: 201,
-    headers: { "Content-Type": "application/json" }, // Ensure JSON response
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message: "A new note created!" }),
+  };
+}
+
+export async function updateNote(event) {
+  const noteId = event.pathParameters.id; // Get {id} from URL
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: `Note with ID ${noteId} updated!` }),
   };
 }
